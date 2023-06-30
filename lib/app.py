d324a5bbd36f8ccd87388cf3cd289b819d0566bb
ottoak <ottoak41@gmail.com>
2023-06-30 19:52:20 -0400

    updated plot component

--- lib/app.py
+++ lib/app.py
@@ -1,10 +1,11 @@
 from dash import Dash, html, Input, Output, callback
 import dash_mantine_components as dmc
 
-from circuit import circuit
-from viz import plot_blochs, mpl_to_img_src, draw_circuit
+from circuit import states
+from viz import plot_blochs_v2, draw_circuit
 
-figs = [mpl_to_img_src(f) for f in plot_blochs(circuit)]
+state_labels = list(states.keys())
+state_spheres = {state: plot_blochs_v2(states[state]) for state in states.keys()}
 
 with open("lib/circuit.py") as f:
     lines = f.readlines()
@@ -13,7 +14,7 @@ app = Dash()
 
 code_card = dmc.Card(
     dmc.Prism(
-        children="".join(lines[5:]),
+        children="".join(lines[5:-2]),
         language="python",
         withLineNumbers=True,
     )
@@ -30,15 +31,13 @@ selector = dmc.NumberInput(
     label="Circuit Step",
     value=0,
     min=0,
-    max=len(figs) - 1,
+    max=len(states) - 1,
     step=1,
     style={"width": 250},
     id="selector",
 )
 
-state_viz = html.Div(
-    id="state-viz",
-)
+state_viz = dmc.Grid(id="state-viz", children=[])
 
 app_cols = [
     dmc.Col(
@@ -60,13 +59,37 @@ app.layout = html.Div(
     Output(component_id="state-viz", component_property="children"),
     Input(component_id="selector", component_property="value"),
 )
-def show_state(sel_state):
-    return dmc.Card(
-        dmc.Image(
-            src=figs[sel_state],
-            width="100%",
+def show_state(sel_state: int):
+    children = []
+
+    for i, fig_src in enumerate(state_spheres[state_labels[sel_state]]):
+        card = dmc.Card(
+            children=[
+                dmc.Center(
+                    dmc.Badge(f"q[{i}]", color="red", variant="light", size="lg")
+                ),
+                dmc.Center(
+                    dmc.Image(
+                        src=fig_src,
+                        style={"width": "75%", "height": "75%"},
+                    ),
+                    mt=10,
+                ),
+            ],
+            withBorder=True,
+            shadow="sm",
+            radius="md",
+            style={"width": "90%", "height": 250},
         )
-    )
+
+        children += [
+            dmc.Col(
+                card,
+                span=6,
+            )
+        ]
+
+    return children
 
 
 if __name__ == "__main__":
